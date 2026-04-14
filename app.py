76be7c8a85755eb8187a144ba886955cf8a57407
Mallu1296 <144100520+Mallu1296@users.noreply.github.com>
2026-04-15 00:13:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,9 +16,9 @@ app = Flask(__name__)
 app.secret_key = 'super_secret_key_change_this_later'
 
 # This pulls the values from the Render "Environment" tab we set up
-TWILIO_SID = os.environ.get('ACf89606c16e2fc08e52786dba096aa77c')
-TWILIO_AUTH_TOKEN = os.environ.get('cd2b7818b86b3ef3bcb17d641a442aa2')
-TWILIO_PHONE = os.environ.get('+15755868791')
+TWILIO_SID = os.environ.get('TWILIO_ACCOUNT_SID')
+TWILIO_AUTH_TOKEN = os.environ.get('TWILIO_AUTH_TOKEN')
+TWILIO_PHONE = os.environ.get('TWILIO_PHONE_NUMBER')
 
 client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
 
